69b6dddcae7a69f6f14332eca4dbf5270ca47f11
ZheShi <zhe.shi@uni-ulm.de>
2026-02-13 17:06:31 +0100

    restructure

--- Meta-SDK_Samples-main/Assets/Scripts/MainMenuScripts/SimpleLoader.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public class SimpleLoader : MonoBehaviour
-{
-    void Start()
-    {
-        // Force load immediately without waiting for UI or events
-        GetComponent<OVRSceneManager>().LoadSceneModel();
-    }
-}
